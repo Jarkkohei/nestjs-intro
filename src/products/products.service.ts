086ddf4378e89b9884500ad652d12549efb9988c
Jarkko <jarkkohei@luukku.com>
2019-07-10 18:11:40 +0300

    Implement updating product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -22,6 +22,25 @@ export class ProductsService {
         return {...product};
     }
 
+    updateProduct(productId: string, title: string, description: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = { ...product };
+
+        if (title) {
+            updatedProduct.title = title;
+        }
+
+        if (description) {
+            updatedProduct.description = description;
+        }
+
+        if (price) {
+            updatedProduct.price = price;
+        }
+
+        this.products[index] = updatedProduct;
+    }
+
     private findProduct(productId: string): [Product, number] {
         const productIndex = this.products.findIndex((prod) => prod.id === productId);
         const product = this.products[productIndex];
