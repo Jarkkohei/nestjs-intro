61b61dc99cc5e8c783f50f436aa0423b9e4046c8
Jarkko <jarkkohei@luukku.com>
2019-07-11 14:31:45 +0300

    Inject the Product model into the ProductsService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
     private products: Product[] = [];
+
+    constructor(
+        @InjectModel('Product') private readonly productModel: Model<Product>
+    ) {}
 
     insertProduct(title: string, description: string, price: number) {
         const id = Math.random().toString();
