708c098a8bbd84faddd8261e76c501be69046f60
Jarkko <jarkkohei@luukku.com>
2019-07-11 14:12:48 +0300

    Clean up redundant commented out examples

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,20 +1,5 @@
 export class Product {
 
-    /*
-    id: string;
-    title: string;
-    description: string;
-    price: number;
-
-    constructor(id: string, title: string, description: string, price: number) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-    }
-    */
-
-    // An alternative way to populate class properties.
     constructor(
         public id: string, 
         public title: string, 
